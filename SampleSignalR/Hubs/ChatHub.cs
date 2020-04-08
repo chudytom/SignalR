5571ead0b5f7709a6bc021fb3829ecad3a40afb3
chudytom <mail.chudzik@gmail.com>
2020-04-08 14:02:08 +0200

    Simple chat example working

---
+++ SampleSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private const string ReceiveStringMethod = "ReceiveMessage";
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync(ReceiveStringMethod, user, message);
+        }
+    }
+}
---
+++ SampleSignalR/wwwroot/js/chat.js
@@ -0,0 +1,28 @@
+﻿"use strict";
+
+let connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    const decodedMessage = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    const fullMessage = user + " says " + decodedMessage;
+    const listItem = document.createElement("li");
+    listItem.textContent = fullMessage;
+    document.getElementById("messagesList").appendChild(listItem);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (error) {
+    console.error(error.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    const user = document.getElementById("userInput").value;
+    const message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (error) {
+        console.error(error.toString());
+    });
+    event.preventDefault();
+});
